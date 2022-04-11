af2cd48b4c7e31a11de5a503dc7103d38f681029
Enminsk <enminsk@yandex.ru>
2022-04-12 00:33:09 +0300

    add weather app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { debounce } from 'lodash';
+import { getCurrentWeather } from '../api';
+//import { LOAD_STATUSES } from '../constants';
 
 
-export const App = () => {
-    return 
+export class App extends React.Component {
+    state = {
+        city: '',
+        data: { },
+    }
+
+/*     fetchWeather(params) {
+        this.setState({ loadStatus: LOAD_STATUSES.LOADING });
+
+        getCurrentWeather(params)
+            .then(( main ) => {
+                this.setState({ loadStatus: LOAD_STATUSES.LOADED, data: main });
+            }).catch(() => {
+                this.setState({ loadStatus: LOAD_STATUSES.ERROR, data: {} });
+            });
+    } */
+
+    fetchWeatherDebounce = debounce(getCurrentWeather, 1000)
+
+    componentDidUpdate(_, prevState) {
+        if (prevState.city !== this.state.city) {
+            this.fetchWeatherDebounce({ city: this.state.city })
+        }
+    }
+
+    render() {
+        return <div>
+            <input value={this.state.city} onChange={(e) => this.setState({ city: e.target.value })} />
+        </div>
+    }
 }
